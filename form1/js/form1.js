973dcd0f4ea4f6e81eff7c6d4f545a3752db2492
JT <45980465+JTX94X@users.noreply.github.com>
2020-04-13 16:58:23 +0200

    initial commit

---
+++ form1/js/form1.js
@@ -0,0 +1,36 @@
+$(function(){
+    $("#wizard").steps({
+        headerTag: "h4",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        transitionEffectSpeed: 300,
+        labels: {
+            next: "Next",
+            previous: "Back",
+            finish: "Create"
+        }
+    });
+    $('.wizard > .steps li a').click(function(){
+        $(this).parent().addClass('checked');
+        $(this).parent().prevAll().addClass('checked');
+        $(this).parent().nextAll().removeClass('checked');
+    });
+    // Custom Button Jquery Steps
+    $('.forward').click(function(){
+        $("#wizard").steps('next');
+    })
+    $('.backward').click(function(){
+        $("#wizard").steps('previous');
+    })
+})
+$(function () {
+  $('#underlyings').each(function () {
+    $(this).select2({
+      theme: 'bootstrap4',
+      width: 'style',
+      placeholder: $(this).attr('placeholder'),
+      allowClear: Boolean($(this).data('allow-clear')),
+    });
+  });
+});
